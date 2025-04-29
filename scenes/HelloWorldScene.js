abe865cce5ea6877843c8c2f15dd481721bd91b4
juanvh241 <samsungy201533@gmail.com>
2025-04-29 19:42:34 -0300

    limpieza y avance fondo

--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -2,45 +2,25 @@
 
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
-    // key of the scene
-    // the key will be used to start the scene by other scenes
-    super("hello-world");
+    super("Helloword")
   }
 
   init() {
-    // this is called before the scene is created
-    // init variables
-    // take data passed from other scenes
-    // data object param {}
   }
 
   preload() {
     // load assets
-    this.load.image("sky", "./assets/space3.png");
-    this.load.image("logo", "./assets/phaser3-logo.png");
-    this.load.image("red", "./assets/particles/red.png");
+    this.load.image ("Cielo","public/assets/Cielo.webp")
   }
 
   create() {
     // create game objects
-    this.add.image(400, 300, "sky");
+    this.add.image (400, 300, "Cielo").setScale (2);
+    }
 
-    const logo = this.physics.add.image(400, 100, "logo");
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    // emmit particles from logo
-    const emitter = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
-    });
-
-    emitter.startFollow(logo);
+    update() {
+      // update game objects
+    }
   }
 
-  update() {
-    // update game objects
-  }
-}
+
